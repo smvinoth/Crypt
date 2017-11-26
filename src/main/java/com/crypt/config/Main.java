42a4035b1570ca7ba2572d037dc00abcd52c645a
Vinoth <vinsm@paypal.com>
2017-11-26 15:42:47 +0530

    Procfile

--- src/main/java/com/crypt/config/Main.java
+++ src/main/java/com/crypt/config/Main.java
@@ -10,6 +10,7 @@ public class Main
 {
      public static void main(String[] args) 
      {
+    	 System.out.println("Application Started!");
          SpringApplication.run(Main.class, args);
      }
 }
